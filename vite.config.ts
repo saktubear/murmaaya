22201de79daaeb4719e970493291092d7451a0ad
saktubear <harshsvs5765@gmail.com>
2026-09-11 19:24:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/murmaaya/",
   server: {
     host: "::",
     port: 8080,
